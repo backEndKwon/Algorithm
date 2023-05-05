1791ae861e82cf10e763bd3ad0e7c37bbafa1563
backEndKwon <rhaehfdl0433@gmail.com>
2023-05-05 20:03:37 +0900

    05,May 2023

--- app.js
+++ app.js
@@ -1,32 +1,51 @@
-// 저주의 숫자 3
-//71%
-// 3x 마을 사람들은 3을 저주의 숫자라고 생각하기 때문에 3의 배수와 숫자 3을 사용하지 않습니다. 3x 마을 사람들의 숫자는 다음과 같습니다.
-
-// 10진법	3x 마을에서 쓰는 숫자	10진법	3x 마을에서 쓰는 숫자
-// 1	1	6	8
-// 2	2	7	10
-// 3	4	8	11
-// 4	5	9	14
-// 5	7	10	16
+// 소인수 분해
+//68%
 
 // n	result
-// 15	25
-// 40	76
+// 12	[2, 3]
+// 17	[17]
+// 420	[2, 3, 5, 7]
+//전략 : 0으로 나누는값이 있으면 그값을 ANSWR에 넣어준다
+삽질
+// function solution(num) {
+//   let answer = [];
+//   let n = num;
+//   for (let i = 2; i <= n; i++) {
+//     if (isPrime(i) && Number.isInteger(n / i)) {
+//       answer.push(i);
+//       n /= i;
+//     }
+//   }
+//   return answer;
+// }
 
-//전략 : 3의 배수&&3이 들어간수를 뺸수를 나열하면됌(1부터 100까지)
-//10진법은 index로 활용
-
-//13같은애들 어떻게 할까...13 23 43 53  73 83 
+// function isPrime(num) {
+//   if (num === 1) return false;
+//   for (let i = 2; i < n; i++) {
+//     if (num % i === 0) return false;
+//   }
+//   return true;
+// }
 function solution(n) {
-  let answer = [];
-  for (let i = 1; i <= 300; i++) {
-    if (i % 3 !== 0 && !i.toString().includes('3') ) {
-      answer.push(i);
-    }
+  let ans = [];
+  let num = n;
+  for (let i = 2; i <= num; i++) {
+      if (isPrime(i) && Number.isInteger(num / i)) {
+          ans.push(i);
+          num /= i;
+      }
   }
+  return ans;
+}
 
-  return answer[n-1];
+function isPrime(n) {
+  if (n === 1) return false;
+  for (let i = 2; i < n; i++) {
+      if (n % i === 0) return false;
+  }
+  return true;
 }
 //키포인트 i.toString().includes('3')
-console.log(solution(15));
-console.log(solution(40));
+console.log(solution(2));
+console.log(solution(17));
+console.log(solution(420));
