982f50759d59411ffbd725c3adcb3bb72d0f8046
backEndKwon <rhaehfdl0433@gmail.com>
2023-05-16 22:48:34 +0900

    May 16, 2023 : 문자열 정렬 sort()

--- test.js
+++ test.js
@@ -1,28 +1,20 @@
-//가위바위보
+//한번만 등장한 문자
 
-//88%
-// 가위 2, 바위 0 보 5
-// rsp	result
-// "2"	"0"
-// "205"	"052"
-// 주어지는 값은 문자열
+//84%
+//   s	        result
+// "abcabcadc"	  "d"
+// "abdc"	      "abcd"
+// "hello"	      "eho"
 
 // 전략 : 난이도 0인만큼 한줄 return 최대한 고민해보기
 //         이기는 것이 아니라 2=0, 0=5, 5=2 로 생각해보기
 function solution(n) {
-return String(n.split('').map((a)=>a==="2"?0 : a==="0"? 5 : 2).join(''))
+let answer = []
+  n.split('').map((a)=> n.split(a).length===2 ? answer.push(a):false )
+return answer.sort().join('')
 }
 
-//다른사람 풀이 객체형
-//개념은 똑같다 2는 0 0은 5 로 대칭
-function solution(n){
-  let arr = {
-    2:0,
-    0:5,
-    5:2
-  }
-  let answer = [...n].map((x)=>arr[x]).join('')
-}
-console.log(solution( "2"));
-console.log(solution("205"))
+console.log(solution( "abcabcadc"));
+console.log(solution("abdc"))
+console.log(solution("hello"))
 
