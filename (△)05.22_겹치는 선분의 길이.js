6a8bef01ac3c317f59bc8485d57b157f2fff959b
backEndKwon <rhaehfdl0433@gmail.com>
2023-05-23 00:38:59 +0900

    May 22, 2023 :반복문자.repeat(반복횟수)

---
+++ (△)05.22_겹치는 선분의 길이.js
@@ -0,0 +1,83 @@
+//겹치는 선분의 길이
+
+//55%
+// // lines	result
+// [[0, 1], [2, 5], [3, 9]]	2
+// [[-1, 1], [1, 3], [3, 9]]	0
+// [[0, 5], [3, 9], [1, 10]]	8
+
+// 전략 : lines의 각 요소들의 길이에 대한 요소값들을 전부 구해서 중복값만 남기기
+// function solution(lines) {
+//   let [a, b, c] = lines;
+//   let a1 = [];//[ 0, 1 ]
+//   let b1 = [];//[ 2, 3, 4, 5 ]
+//   let c1 = [];//[ 3, 4, 5, 6, 7, 8, 9 ]
+
+//   for (let i = a[0]; i <= a[1]; i++) {
+//     a1.push(i);
+//   }
+//   for (let i = b[0]; i <= b[1]; i++) {
+//     b1.push(i);
+//   }
+//   for (let i = c[0]; i <= c[1]; i++) {
+//     c1.push(i);
+//   }
+//   let z = [...a1, ...b1, ...c1]
+//   let answer =  [...new Set(z.filter((x,index)=>z.indexOf(x)!==index))].sort((a,b)=>a-b)
+//   if(answer[1]===answer[0]+1){
+//     return answer.length-1
+//   }else{
+//     return 0
+//   }
+
+// }
+//실패
+
+//전략 2 : 각 0번재 값중 최댓값, 1번째깞중 최댓값 구하기
+function solution(lines) {
+  let start = lines.map((x) => (x = x[0])); //[ 0, 2, 3 ]
+  let end = lines.map((x) => (x = x[1])); //[ 1, 5, 9 ]
+
+  let single = 0;
+  let double = 0;
+  for (let i = Math.min(...start); i < Math.max(...end); i++) {
+    //i는 0~9까지 돈다
+    for (let j = 0; j < lines.length; j++) {
+    //j는 3회 돈다 
+    //ex. i=0// j=0 일때 성립 single=1, j=1일때 성립 single=2 그려면 double= 1, single초기화, j=2일때 성립x
+    //ex i=1// j=0 일때 성립 성립 x , j=1일때 성립 o, j=2일때 성립  o dobule = 1,
+    //ex i=2// j=0 일때 성립 성립 x, j=1일때 성립o , j=2일때 성립 성립 o > dobule =1
+    //ex i=3// j=0일때 성립x, 1  
+    if (i >= start[j] && i < end[j]) {
+        single++;
+      }
+    }
+    if (single >= 2) {
+      double++;
+    }
+    single = 0;//single 초기화
+  }
+  return double;
+}
+
+console.log(
+  solution([
+    [0, 1],
+    [2, 5],
+    [3, 9],
+  ])
+);
+console.log(
+  solution([
+    [-1, 1],
+    [1, 3],
+    [3, 9],
+  ])
+);
+console.log(
+  solution([
+    [0, 5],
+    [3, 9],
+    [1, 10],
+  ])
+);
