3ad8e122f70b0df11fd114fad9108f9c4385952a
backEndKwon <rhaehfdl0433@gmail.com>
2023-07-06 22:55:38 +0900

    July 06, 2023 :toString이진수, Array.from():문자열에서 배열로

--- test.js
+++ test.js
@@ -1,86 +1,42 @@
 /*
-영어끝말잇기
+점프와 순간 이동
+
 lv2
 
-제한 사항
-끝말잇기에 참여하는 사람의 수 n은 2 이상 10 이하의 자연수입니다.
-words는 끝말잇기에 사용한 단어들이 순서대로 들어있는 배열이며, 길이는 n 이상 100 이하입니다.
-단어의 길이는 2 이상 50 이하입니다.
-모든 단어는 알파벳 소문자로만 이루어져 있습니다.
-끝말잇기에 사용되는 단어의 뜻(의미)은 신경 쓰지 않으셔도 됩니다.
-정답은 [ 번호, 차례 ] 형태로 return 해주세요.
-만약 주어진 단어들로 탈락자가 생기지 않는다면, [0, 0]을 return 해주세요.
 
-n	words	result
-3	["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]	[3,3]
-5	["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]	[0,0]
-2	["hello", "one", "even", "never", "now", "world", "draw"]	[1,3]
+
+- 제한 사항 -
+숫자 N: 1 이상 10억 이하의 자연수
+숫자 K: 1 이상의 자연수
+
+- 입출력 예 - 
+N	result
+5	2
+6	2
+5000	5
 
 전략
-n크기의 배열 세팅 word를 1개씩 push하기, 해당 끝글자와 다음차례의 첫글자 비교해서 true? 계속, false? 해당 값을 가졌던 사람 체크(words.length%n으로 구분) 변수 sum을 한텀마다 더해주기
+Hint = 순간이동= 2배수에만 존재 가능, 이동한 지점이 홀수? = 점프했다, 점프는 홀수, 짝수다 있을 수도 
 */
-
-function solution(n, words) {
-  // const set = new Array(n);
-
-  for (let i = 0; i < words.length - 1; i++) {
-    //   set[0] = words[0];
-    //   if (
-    //     words[i][words[i].length - 1] === words[i + 1][0] &&
-    //     !set.includes(words[i + 1])
-    //   ) {
-    //     set[i + 1] = words[i + 1];
-    //   } else {
-    //     break;
-    //   }
-    // }
-    // return Math.ceil(set.length%n+1); //=> 몇번째 사람이 틀렸는지
-    // return Math.ceil(set.length/n); // => 몇번째 게임인지
-    // return words.length === set.length
-    //   ? [0, 0]
-    //   : [(set.length % n) + 1, Math.floor((set.length / n) + 1)];
-    if (
-      words[i][words[i].length - 1] !== words[i + 1][0] ||
-      words.slice(0, i + 1).includes(words[i + 1])
-    ) {
-      return [((i + 1) % n) + 1, Math.floor((i + 1) / n + 1)];
+function solution(n) {
+  let answer = 0;
+  while (n > 0) {
+    if (n % 2 === 0) {
+      n /= 2;
+    } else {
+      --n;
+      answer++;
     }
   }
-  return [0, 0];
+  return answer;
+}
+//--다른사람풀이 이진수 접근--//미쳤다...
+function solution(n) {
+  if (n === 1) return 1;
+  const array = Array.from(n.toString(2)); //Array.from은 문자열을 다 나눠서 배열로 만듬
+  return array.reduce((a, b) => +a + +b);
 }
 
-console.log(
-  solution(3, [
-    "tank",
-    "kick",
-    "know",
-    "wheel",
-    "land",
-    "dream",
-    "mother",
-    "robot",
-    "tank",
-  ])
-); //3이 3번째 tank틀림
-console.log(
-  solution(5, [
-    "hello",
-    "observe",
-    "effect",
-    "take",
-    "either",
-    "recognize",
-    "encourage",
-    "ensure",
-    "establish",
-    "hang",
-    "gather",
-    "refer",
-    "reference",
-    "estimate",
-    "executive",
-  ])
-);
-console.log(
-  solution(2, ["hello", "one", "even", "never", "now", "world", "draw"])
-);
+console.log(solution(5));
+console.log(solution(6));
+console.log(solution(5000));
