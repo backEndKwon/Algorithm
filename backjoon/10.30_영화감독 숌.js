6106061f167e5fa08c70294f715e1baaf7d98c19
studio-kyj <rhaehfdl0433@gmail.com>
2023-10-30 21:11:40 +0900

    feat: Oct 30, 2023 : 영화감독 숌

--- backjoon/10.30_영화감독 숌.js
+++ backjoon/10.30_영화감독 숌.js
@@ -1,14 +1,16 @@
 /* 
-1436번 영화감독 숌 */
-/* 
-문제이해
+1436번 영화감독 숌
+
+[문제이해]
 666 1666 2666 3666 ...5666 6660 6661 ~
 이런식으로 추가
 
-접근방법
+[접근방법]
 무식한 방법으로 666부터 +1 해서 666이 포함되면 answer +1
 해당 answer가 n과 같아지면 출력
 
+[시간/공간복잡도]
+예상 = bigO(n) 
  */
 const fs = require("fs");
 const backData = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
@@ -18,11 +20,11 @@ const movie = (n) => {
   let title = 0;
   let max = Infinity;
   let count = 0;
-  for (let i = 666; i < max; i++) {
-    if (i.toString().includes(666)) {
+  for (let i = 666; i < max; i++) { // bigO(n)
+    if (i.toString().includes(666)) { // bigO(1)
       count++;
     }
-    if (count === Number(n)) {
+    if (count === Number(n)) {// bigO(1)
       title = i;
       break;
     }
